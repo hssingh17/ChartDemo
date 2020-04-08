86732454725b8a8a7c6e7ce03d4b2ce5c0c0eb22
Hoshiyar Singh <Hoshiyar.Singh@live.com>
2020-04-08 13:42:41 +0530

    Add project files.

---
+++ ChartDemo/Controllers/ChartController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ChartDemo.Controllers
+{
+    public class ChartController : Controller
+    {
+        public IActionResult Area()
+        {
+            return View();
+        }
+        public IActionResult Pie()
+        {
+            return View();
+        }
+        public IActionResult Scatter()
+        {
+            return View();
+        }
+    }
+}
